0a2d3011b5389c2a09f5dc002b24561b4170d0de
leigharobinson <lmarobinson24@gmail.com>
2020-11-12 16:41:52 -0600

    chapter 1 from book 2

---
+++ sql-professional/Book2_Chap1.sql
@@ -0,0 +1,83 @@
+SELECT
+	last_name, first_name, city, state
+FROM
+	customers
+WHERE
+	state = 'TX';
+	
+SELECT
+	last_name, first_name, city, state
+FROM
+	customers
+WHERE
+	city = 'Houston' AND state = 'TX';
+	
+SELECT
+	last_name, first_name, city, state
+FROM
+	customers
+WHERE
+	state = 'TX' OR state = 'TN';
+
+SELECT
+	last_name, first_name, city, state
+FROM
+	customers
+WHERE
+	state IN ('TX', 'TN', 'CA');
+	
+SELECT
+	last_name, first_name, city, state
+FROM
+	customers
+WHERE
+	state LIKE 'C%';
+	
+SELECT
+	last_name, first_name
+FROM
+	customers
+WHERE
+	LENGTH(last_name) > 5 AND LENGTH(first_name) <= 7;
+	
+SELECT
+	last_name, first_name, company_name
+FROM
+	customers
+WHERE
+	company_name IS NULL;
+	
+-- Book 2 Chap 1 Questions
+-- 1. Get a list of sales records where the sale was a lease.
+
+SELECT *
+FROM sales
+WHERE sales_type_id = 3;
+
+
+-- 2. Get a list of sales where the purchase date is within the last two years.
+
+SELECT *
+FROM sales
+WHERE 
+purchase_date between '2017/01/01' AND '2020/01/01';
+
+-- 3. Get a list of sales where the deposit was above 
+--5000 or the customer payed with American Express.
+
+SELECT *
+FROM sales
+WHERE payment_method Like 'americane%' AND deposit >= 5000;
+
+-- 4. Get a list of employees whose first names start with "M" or ends with "E".
+
+SELECT *
+FROM employees
+Where first_name like 'M%' AND last_name like lower('%E');
+
+-- 5. Get a list of employees whose phone numbers have the 600 area code.
+-- the answer is going to be none 
+
+SELECT *
+FROM employees
+Where phone like '600%';
